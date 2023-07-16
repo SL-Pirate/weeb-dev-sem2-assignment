c449df430782ce96a7585ebd8667b5645219a096
Isira Herath <isira.20223184@iit.ac.lk>
2023-07-17 00:09:41 +0530

    Implemented comments page

---
+++ scripts/comments.js
@@ -0,0 +1,79 @@
+function validateNotNull(value, id) {
+    if (value == null) {
+        document.getElementById(id).style.display = "block";
+        return false;
+    }
+
+    document.getElementById(id).style.display = "none";
+    return true;
+}
+
+function getRating () {
+    let val = null;
+    const elements = document.getElementsByName("satisfaction-value");
+    elements.forEach(element => {
+        if (element.checked) {
+            val = element.value;
+        }
+    });
+
+    console.log(val);
+    return val;
+}
+
+function getMoreDetails () {
+    const moreDetails = document.getElementById("tellmore-text").value;
+
+    if (moreDetails == '') {
+        return null;
+    }
+    
+    return moreDetails;
+}
+
+function getTaskValueById(id) {
+    const value = document.getElementById(id).value;
+
+    if (value == "none") {
+        return null;
+    }
+
+    return value;
+}
+
+function sendMail () {
+    const rating = getRating();
+    const moreDetails = getMoreDetails();
+    const taskToday = getTaskValueById("task-today");
+    const taskAgain = getTaskValueById("task-again");
+
+    if (validateNotNull(rating, "rate-required") 
+    & validateNotNull(moreDetails, "tellmore-required")) {
+        const emailAddr = "isira.20223184@iit.ac.lk";
+        const subject = "Feedback for Travel Sri Lanka website";
+        let body = 
+            "Hello Isira,\n" +
+            "I rate my experience with your site " + 
+            rating +
+            "/10\n" +
+            moreDetails +
+            "\n";
+
+        if (taskToday != null) {
+            body += "I used " +
+            taskToday + 
+            " to complete this task.\n"
+        }
+        if (taskAgain != null) {
+            body += "I will be using " +
+            taskAgain + 
+            " to complete this task when I do again\n"
+        }
+
+        body += "Thank you for creating and maintaining this site!";
+
+        let email = "mailto:" + emailAddr + "?subject='" + subject + "'&body='" + body + "'";
+        console.log(email);
+        window.open(email, '_blank');
+    }
+}
