845d75eb1be2aa47a4d10dd6d12b06a2f414a317
Shanaka2002 <shanakasamararathne@gmail.com>
2023-08-05 11:22:21 +0530

    updated styling for about-us page

--- scripts/about-us.js
+++ scripts/about-us.js
@@ -1,6 +1,10 @@
 function displayDetails(name, role) {
     document.getElementById('details').innerHTML = name + ', ' + role;
+    document.getElementById('details').style.backgroundColor = "#3d3d3d";
   }
   function clearDetails() {
+    document.getElementById('details').style.backgroundColor = "#ffffff";
     document.getElementById('details').innerHTML = '';
   }
+
+clearDetails();
